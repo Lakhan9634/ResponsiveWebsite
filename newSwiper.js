940582401c294a091f43da36f4b5a6e2f0ad333c
Lakhan9634 <lakhanrajput96346@gmail.com>
2024-06-21 12:41:35 +0530

    Add files via upload

---
+++ newSwiper.js
@@ -0,0 +1,16 @@
+let newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: 'true',
+
+
+    breakpoints: {
+        576: {
+            slidePerView: 2,
+        },
+        768:{
+            slidePerView: 3,
+        },
+    },
+});
+
+
---
+++ testimonialSwiper.js
@@ -0,0 +1,8 @@
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
